54a6f202aff905976e72f3251bbbc2d935cb3eb1
최승은 <liberalfilm@gmail.com>
2021-12-08 16:36:39 +0900

    2021.12.8(수) 16:38 byte Stream

--- src/com/javaex/ex01/Ex01.java
+++ src/com/javaex/ex01/Ex01.java
@@ -10,38 +10,28 @@ import java.io.OutputStream;
 public class Ex01 {
 	public static void main(String[] args) throws Exception {
 
-		
-				
 		InputStream in = new FileInputStream("C:\\javaStudy\\file\\img.jpg");
 		// 파일이 없을 때를 대비해서 예외처리 try-catch를 해줘야 함
 		OutputStream out = new FileOutputStream("C:\\javaStudy\\file\\byteimg.jpg");
 
 		byte[] buff = new byte[1024];
 		System.out.println("복사를 시작합니다.");
-		while(true) {
-			int data = in.read(buff);
-			
-			if(data == -1) {
-				System.out.println("복사가 완료되었습니다." + data);
-				break;
-			}
-			out.write(buff);
-		}
-		
-		
-		/*
-		System.out.println("복사를 시작합니다.");
 		while (true) {
-			int data = in.read();
+			int data = in.read(buff);
 
 			if (data == -1) {
 				System.out.println("복사가 완료되었습니다." + data);
 				break;
 			}
-			out.write(data);
-		*/
-		
-		
+			out.write(buff);
+		}
+
+		/*
+		 * System.out.println("복사를 시작합니다."); while (true) { int data = in.read();
+		 * 
+		 * if (data == -1) { System.out.println("복사가 완료되었습니다." + data); break; }
+		 * out.write(data);
+		 */
 
 	}
 
