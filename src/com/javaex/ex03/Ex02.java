05dc5855e4fa115450bd9288cfd83372dd81a0e3
최승은 <liberalfilm@gmail.com>
2021-12-09 10:50:31 +0900

    2021.12.9(목)10:50 character buffered stream

---
+++ src/com/javaex/ex03/Ex02.java
@@ -0,0 +1,29 @@
+//2021.12.9(목)10:30수업, 01.I/O Stream중 character buffered stream
+
+package com.javaex.ex03;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class Ex02 {
+
+	public static void main(String[] args) throws IOException {
+
+		Reader fr = new FileReader("C\\javaStudy\\file\\song.txt");
+		BufferedReader br = new BufferedReader(fr);
+
+		while (true) {
+			String str = br.readLine(); // 한 줄씩 읽어온다: 줄바꿈은 제외함
+			if (str == null) {
+				break;
+			}
+
+			System.out.println(str);
+		}
+
+		br.close();
+	}
+
+}
